7d3b4951ef3e2df42f5e4f58c439ad9584e5e97b
1Gatik1 <gatik0493.be21@gmail.com>
2022-04-12 11:47:31 +0530

    Commiting the member function addition|hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,21 @@
 #include<iostream>
 using namespace std
+class Stu
+{
+	private:
+		int roll,age;
+	public:
+		Stu(){}
+		void getdata(){}
+		void putdata(){}
+};
 int main(){
 	cout<<"hello world"<<endl;
+// last comment
 }
+//ading function def here
+void Stu::getdata()
+{
+}
+void Stu::putdata()
+{}
